b18ba00150fd0b2055d0b27823d73d97e115b13c
Daniel Morales <daniel.morales23@outlool.cl>
2023-12-14 17:33:57 -0300

    FIX CSS AND LOGIC

--- src/logic/filterEmoji.js
+++ src/logic/filterEmoji.js
@@ -1,13 +1,19 @@
-import emojiList from '../helpers/emojiList.json'
+import emojiList from '../helpers/emojiList.json';
 
-export default function filterEmojis(searchText, maxResult){
-    
-    return emojiList.filter(emoji=>{
+export default function filterEmojis(searchText, maxResult) {
+    return emojiList.filter(emoji => {
+        const lowercaseSearchText = searchText.toLowerCase();
+        
+        // Filtra por título
+        if (emoji.title.toLowerCase().includes(lowercaseSearchText)) {
+            return true;
+        }
 
-        if(emoji.title.toLowerCase().includes(searchText.toLowerCase())) return true;
-
-        if(emoji.keywords.includes(searchText)) return true;
+        // Filtra por palabras clave (si está definido y es un array)
+        if (Array.isArray(emoji.keywords) && emoji.keywords.some(keyword => keyword.includes(lowercaseSearchText))) {
+            return true;
+        }
 
         return false;
-    }).slice(0, maxResult)
+    }).slice(0, maxResult);
 }
